00064b254a092e6f1ae731b5baca162f6194d6ea
Ricky Oswald <rickyoswald@gmail.com>
2016-07-02 15:21:35 +0100

    prettyfying

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<?php
-	session_start();
-	
-	include_once("db.php");
-	
-	if(isset($_POST['title']) && isset($_POST['note']) && $_POST['title'] != "" && $_POST['note'] != "") {
-		$title = $_POST['title'];
-		$note = $_POST['note'];
-		$tags = $_POST['tags'];
-		$sql_store = "INSERT into notes (id, title, note, tags) VALUES (NULL, '$title', '$note', '$tags')";
-		$sql = mysqli_query($db, $sql_store) or die(mysql_error());
-		echo "Note '$title' submitted.";
-	} else {
-		echo "Enter Title and Note.";
-	}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -25,53 +8,9 @@
 
 <body>
 
-	<h1>New Note</h1>
-	<form action="index.php" method="POST">
-		<input type="text" name="title" value="" placeholder="Note Title" />
-		<input type="text" name="note" value="" placeholder="Note" />
-		<input type="text" name="tags" value="" placeholder="Tags" />
-		<input type="submit" name="submit" value="Submit" />
-	</form>
-	
-	<br />
-	
-	<h1>Notes</h1>
-	<table border='1'>
-		<tr>
-			<th>ID</th>
-			<th>Title</th>
-			<th>Tags</th>
-			<th>Date</th>
-		</tr>
-		
-		<?php
-			$sql_list = "SELECT * FROM notes ORDER BY id ASC";
-			$results = mysqli_query($db, $sql_list) or die(mysql_error());
-			$notes = "";
-			
-			if(mysqli_num_rows($results) > 0) {
-				while($row = mysqli_fetch_assoc($results)) {
-					$id = $row['id'];
-					$title = $row['title'];
-					$tags = $row['tags'];
-					$date = $row['date'];
-					
-					$notes .= "<tr><td>$id</td><td>$title</td><td>$tags</td><td>$date</td><td><a href='note.php?id=$id'>Open</a></td></tr>";
-				}
-				
-				echo $notes;
-			} else {
-				echo "No resutls";
-			}
-		?>
-	</table>
-	<br />
-
-	<button type="button" onclick="document.getElementById('demo').innerHTML = Date()">
-		Click to display date.
-	</button>
-
-	<p id="demo" />
+	<h1>Please Login or Register</h1>
+	<a href="login.php">Login</a> or
+	<a href="register.php">Register</a>
 
 </body>
 </html>
