b3ad5e6c00b2eb901e6e0bf295f02b7a9600cb7a
Ricky Oswald <rickyoswald@gmail.com>
2016-07-02 15:31:56 +0100

    prettyfying and preparing for PDO

--- login.php
+++ login.php
@@ -1,33 +1,3 @@
-<?php
-	session_start();
-	
-	if(isset($_POST['login'])) {
-		include_once("db.php");
-		$username = strip_tags($_POST['username']);
-		$password = strip_tags($_POST['password']);
-		
-		$username = stripslashes($username);
-		$password = stripslashes($password);
-		
-		$username = mysqli_real_escape_string($db, $username);
-		$password = mysqli_real_escape_string($db, $password);
-		
-		$sql = "SELECT * FROM users WHERE username='$username' LIMIT 1";
-		$query = mysqli_query($db, $sql);
-		$row = mysqli_fetch_array($query);
-		$id = $row['id'];
-		$db_password = $row['password'];
-		
-		if(password_verify($password, $db_password)) {
-			$_SESSION['username'] = $username;
-			$_SESSION['id'] = $id;
-			header("Location: index.php");
-		} else {
-			echo "Username/Password not found";
-		}
-	}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -36,16 +6,16 @@
 	<link href='https://fonts.googleapis.com/css?family=Titillium+Web' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	<h1>Login</h1>
-	<form action="login.php" method="post" enctype="multipart/form-data">
-		<input placeholder="Username" name="username" type="text" autofocus>
-		<input placeholder="Password" name="password" type="text">
-		<input name="login" type="submit" value="Login">
-	</form>
-	<br />
+	<div class="header">
+		<a href="/">Notes</a>
+	</div>
 	
-	<form action="register.php">
-		<input type="submit" value="Register">
+	<h1>Login</h1>
+	
+	<form action="login.php" method="POST">
+		<input type="text" placeholder="Enter your email" name="email">
+		<input type="password" placeholder="and password" name="password">
+		<input type="submit">
 	</form>
 </body>
 </html>
--- register.php
+++ register.php
@@ -1,74 +1,3 @@
-<?php
-	// Redirect logged-in users.
-	if(isset($_SESSION['id'])) {
-		header("Location: index.php");
-	}
-	
-	if(isset($_POST['register'])) {
-		include_once("db.php");
-		
-		// Sanitise input
-		$username = strip_tags($_POST['username']);
-		$password = strip_tags($_POST['password']);
-		$password_confirm = strip_tags($_POST['password_confirm']);
-		$email = strip_tags($_POST['email']);
-		$username = stripslashes($username);
-		$password = stripslashes($password);
-		$password_confirm = stripslashes($password_confirm);
-		$email = stripslashes($email);
-		$username = mysqli_real_escape_string($db, $username);
-		$password = mysqli_real_escape_string($db, $password);
-		$password_confirm = mysqli_real_escape_string($db, $password_confirm);
-		$email = mysqli_real_escape_string($db, $email);
-		
-		if($username == "") {
-			echo "Please enter a username.";
-			return;
-		}
-		
-		if($password == "" || $password_confirm == "") {
-			echo "Please enter a password.";
-			return;
-		}
-		
-		// Check the passwords match before hashing
-		if($password != $password_confirm) {
-			echo "Passwords do not match.";
-			return;
-		}
-		
-		if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-			echo "Please enter a valid email.";
-			return;
-		}
-		
-		// Hash the password
-		$password = password_hash($password, PASSWORD_DEFAULT);
-		$password_confirm = password_hash($password_confirm, PASSWORD_DEFAULT);
-		
-		$sql_fetch_username = "SELECT username FROM users WHERE username = '$username'";
-		$sql_fetch_email = "SELECT email FROM users WHERE email = '$email'";
-		
-		$query_username = mysqli_query($db, $sql_fetch_username);
-		$query_email = mysqli_query($db, $sql_fetch_email);
-		
-		if(mysqli_num_rows($query_username)) {
-			echo "Please choose another username.";
-			return;
-		}
-		if(mysqli_num_rows($query_email)) {
-			echo "$email already has an account.";
-			return;
-		}
-		
-		$sql_store = "INSERT into users (username, password, email) VALUES ('$username', '$password', '$email')";
-		
-		mysqli_query($db, $sql_store);
-		
-		header("Location: index.php");
-	}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -77,12 +6,6 @@
 	<link href='https://fonts.googleapis.com/css?family=Titillium+Web' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	<form action="register.php" method="post" enctype="multitype/form-data">
-		<input placeholder="Username" name="username" type="text" autofocus>
-		<input placeholder="Password" name="password" type="password">
-		<input placeholder="Confirm Password" name="password_confirm" type="password">
-		<input placeholder="E-Mail" name="email" type="text">
-		<input name="register" type="submit" value="Register">
-	</form>
+	<h1>Register</h1>
 </body>
 </html>
