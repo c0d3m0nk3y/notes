11ed181092d26bdfdefecd01bf11e1b62d5cef4e
Ricky Oswald <rickyoswald@gmail.com>
2016-07-02 16:52:17 +0100

    migrating to PDO

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 	require 'db.php';
 
 	if(!empty($_POST['username']) && !empty($_POST['password'])) {
-		$records = $db->prepare('SELECT id,username,email,password FROM users WHERE username = :username');
+		$records = $db->prepare('SELECT id,username,password,email FROM users WHERE username = :username');
 		$records->bindParam(':username', $_POST['username']);
 		$records->execute();
 		$results = $records->fetch(PDO::FETCH_ASSOC);
@@ -11,7 +11,7 @@
 		if(count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
 			die('logged in');
 		} else {
-			die("login error $results");
+			die('login error');
 		}
 	}
 
