a7764b90371f56c5b35060e149683e119f1ab560
Ricky Oswald <rickyoswald@gmail.com>
2016-07-02 04:51:19 +0100

    added login and logout templates

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>- Login -</title>
+</head>
+<body>
+	<h1>Login</h1>
+	<form action="login.php" method="post" enctype="multipart/form-data">
+		<input placeholder="Username" name="username" type="text" autofocus>
+		<input placeholder="Password" name="password" type="text">
+		<input name="login" type="submit" value="Login">
+	</form>
+</body>
+</html>
