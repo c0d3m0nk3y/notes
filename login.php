011c562d89fa6f8210379a43f2c81ca2e5c4e3be
Ricky Oswald <rickyoswald@gmail.com>
2016-07-02 05:05:12 +0100

    added login page

--- login.php
+++ login.php
@@ -1,4 +1,33 @@
 <?php
+	session_start();
+	
+	if($_POST['login']) {
+		include_once("db.php");
+		$username = strip_tags($_POST['username']);
+		$password = strip_tags($_POST['password']);
+		
+		$username = stripslashes($username);
+		$password = stripslashes($password);
+		
+		$username = mysqli_real_escape_string($username);
+		$password = mysqli_real_escape_string($password);
+		
+		$password = md5($password);
+		
+		$sql = "SELECT * FROM users WHERE username='$username' LIMIT 1";
+		$query = mysqli_query($db, $sql);
+		$row = mysqli_fetch_array($query);
+		$id = $row['id'];
+		$db_password = $row['password'];
+		
+		if($password == $db_password) {
+			$_SESSION['username'] = $username;
+			$_SESSION['id'] = $id;
+			header("Location: index.php");
+		} else {
+			echo "Username/Password not found";
+		}
+	}
 ?>
 
 <!DOCTYPE html>
