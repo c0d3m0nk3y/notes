c791cb0efa19c8bd20f6faa3799847fb834a0df2
Ricky Oswald <rickyoswald@gmail.com>
2016-07-03 20:35:01 +0100

    added profile link

--- notes.php
+++ notes.php
@@ -5,6 +5,19 @@
 	if(!isset($_SESSION['id'])) {
 		header("Location: index.php");
 	}
+
+	require 'db.php';
+
+	$records = $db->prepare('SELECT id,username,password,email FROM users WHERE id = :id');
+	$records->bindParam(':id', $_SESSION['id']);
+	$records->execute();
+	$results = $records->fetch(PDO::FETCH_ASSOC);
+
+	$username = NULL;
+
+	if(count($results) > 0) {
+		$username = $results;
+	}
 ?>
 
 <!DOCTYPE html>
@@ -21,6 +34,8 @@
 	</div>
 
 	<a href="addnote.php">Add A Note</a>
+	-
+	<a href="profile.php"><?php echo $results['username'] ?></a>
 	-
 	<a href="logout.php">Logout</a>
 
