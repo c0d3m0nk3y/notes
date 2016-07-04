a89bbb47adcd6dda56b6f12264be1ac19532a762
Ricky Oswald <rickyoswald@gmail.com>
2016-07-04 20:01:40 +0100

    added base64 encoded favicon as script

--- index.php
+++ index.php
@@ -7,12 +7,18 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+	<meta charset="utf-8">
+
 	<title>- Notes -</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link href='https://fonts.googleapis.com/css?family=Titillium+Web' rel='stylesheet' type='text/css'>
-	<link rel="icon" type="image/png" href="icon.png">
+	
+	<!--link rel="icon" type="image/png"h ref="icon.png"-->
+	<!-- Suppress browser request for favicon.ico -->
+    <link rel="shortcut icon"type="image/x-icon" href="data:image/x-icon;,">
+    <script src="script.js"></script>
 </head>
 
 <body>
