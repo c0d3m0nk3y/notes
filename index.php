7afcb293c3618836187fa4723aa64dddb0c9e29e
Ricky Oswald <rickyoswald@gmail.com>
2016-07-04 20:12:33 +0100

    foo

--- index.php
+++ index.php
@@ -11,13 +11,13 @@
 <head>
 	<meta charset="utf-8">
 
-	<title>- Notes -</title>
+	<title>Notes</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link href='https://fonts.googleapis.com/css?family=Titillium+Web' rel='stylesheet' type='text/css'>
-	
+
 	<!--link rel="icon" type="image/png"h ref="icon.png"-->
 	<!-- Suppress browser request for favicon.ico -->
-    <link rel="shortcut icon"type="image/x-icon" href="data:image/x-icon;,">
+    <link rel="shortcut icon" type="image/x-icon" href="data:image/x-icon;,">
     <script src="script.js"></script>
 </head>
 
